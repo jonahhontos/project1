5b99bc6cb2c186181438b4d90e59b8597860be2c
Jonah Hontos <jonahhontos@gmail.com>
2016-02-01 15:32:25 -0800

    added declaration of game object + Player, Character, Fighter constructors, and basic attack method

--- js/app.js
+++ js/app.js
@@ -7,3 +7,26 @@ $('body').on("mouseout",".menu-item",function(){
   $(this).toggleClass("menu-item-hover")
   // console.log("mouseout on " + this)
 })
+
+function Player(character) {
+  this.character = character
+}
+
+function Fighter() {
+  this.imagePrefix = "fighter"
+  this.hp = 300
+  this.str = 50
+}
+
+function Character() {
+  this.attack = function(){
+    var attackStrength = Math.floor((Math.random()*(this.str/2)) + this.str * 0.75) 
+    console.log(attackStrength)
+  }
+}
+
+Fighter.prototype = new Character()
+
+var game = {
+  players: [new Player(new Fighter()), new Player(new Fighter())]
+}
