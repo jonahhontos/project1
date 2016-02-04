62a18cf49999545b1564feb38d9896cc01ab3f17
Jonah Hontos <jonahhontos@gmail.com>
2016-02-03 19:20:17 -0800

    added select sound

--- js/app.js
+++ js/app.js
@@ -28,10 +28,13 @@ prelude.play()
     $(this).toggleClass("menu-item-hover")
     // console.log("mouseover on " + this)
   })
-
   $('body').on("mouseout",".menu-item",function(){
     $(this).toggleClass("menu-item-hover")
     // console.log("mouseout on " + this)
+  })
+  $('body').on("mousedown",".menu-item",function(){
+    select.load()
+    select.play()
   })
 
   $('#win-message').hide()
